8b94b52673f84b54e7d9334dac4a36afa5a923b9
Classy-Cassy <122777259+Classy-Cassy@users.noreply.github.com>
2023-03-14 19:33:04 +0700

    Add files via upload

--- shrimp check/shrinp-source.py
+++ shrimp check/shrinp-source.py
@@ -2,6 +2,9 @@ from tkinter import *
 from PIL import ImageTk, Image
 import time
 import random
+import os
+from sys import exit
+
 
 
 # defining the window
@@ -11,13 +14,15 @@ def window():
     global button_exit
     global img
     root = Tk()
+    root.attributes("-topmost", True)
     root.title('Shrimp Check')
-    img = ImageTk.PhotoImage(Image.open(r'Shrimp jpeg path')) #replace here
+    img = ImageTk.PhotoImage(Image.open(os.getcwd()+'\\shrimp-gun.png')) 
     my_label = Label(image=img)
     button_exit = Button(root, text= "Terminate Program", command= exit)
 
+#Show the window
 def jumpscare():
-    root.iconbitmap(r'shrimp ico path') # Replace here
+    root.iconbitmap(os.getcwd()+'\\shrimpicon.ico') 
     my_label.pack()
     button_exit.pack()
     root.bell()
@@ -26,6 +31,6 @@ def jumpscare():
 
 
 while True:
-    time.sleep(random.randint(5,10))
+    time.sleep(random.randint(15,30) * 60) #Change the random time interval here
     window()
     jumpscare()
